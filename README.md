dca17d97d5f57531d27f8d69afc15ab0028d86bf
Valentyna <oreshkav@gmail.com>
2023-05-25 22:49:46 +0200

    addFunctCodeOfWeather

---
+++ README.md
@@ -0,0 +1,14 @@
+    API для определения координат:
+    https://www.geojs.io/docs/v1/endpoints/ip/
+        "https://get.geojs.io/v1/ip/geo.json";
+    
+    API для прогногза погоды:
+    https://open-meteo.com/
+    
+    1. показывал температуру и расшифрованный код
+    2. были стили шрифты картинки 
+    3. деплой на gitPages
+    4. Readme c описанием проекта и ссылкой на pages
+    Alisher Khamidov 18:06
+    https://get.geojs.io/v1/ip/geo.json
+
--- weather.js
+++ weather.js
@@ -1,5 +1,4 @@
 const coordinateUrl = "https://get.geojs.io/v1/ip/geo.json";
-// const weatherUrl = "https://open-meteo.com/"
 const shortWeather = "https://api.open-meteo.com/v1/forecast?";
 
 const weatherCard = document.getElementById("weatherCard");
@@ -21,28 +20,38 @@ async function fetchCoordinate() {
         const latitudeLongitude = "latitude=" + latitude + "&longitude=" + longitude;
 
         // console.log(coordinate);
+        // console.log(urlLink);
 
         const urlLink = `${shortWeather}${latitudeLongitude}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`;
         
-        console.log(urlLink);
+        getWeather(urlLink, city, country, region);
+}
+    catch (error) {
+        console.log("Error fetching weather: ", error);
+    }
+
+    async function getWeather(urlLink, city, country, region) {
+        try{
 
         const responceWeather = await fetch(urlLink);
-        const weather = await responceWeather.json();  
-        const {temperature, windspeed, winddirection, weathercode} = weather.current_weather;
+        const weather = await responceWeather.json();
+        const { temperature, windspeed, winddirection, weathercode } = weather.current_weather;
 
         /**log**/
         // console.log("weather:", weather.current_weather)
+        // console.log(weathercode );
         // console.log(city);
         // console.log(country);
         // console.log(region);
 
-        cityP.innerText = "City: " + city;
+        cityP.innerText = city;
         countryP.innerText = country;
         regionP.innerText = region;
         temperatureP.innerText = "Temperature: " + temperature;
         windspeedP.innerText = "Windspeed: " + windspeed;
         winddirectionP.innerText = "Winddirection: " + winddirection;
-        regionP.weathercodeP = "Weathercode; " + weathercode;
+
+        weathercodeP.innerText = getWeatherByCode(weathercode);
 
     }
     catch (error) {
@@ -50,20 +59,64 @@ async function fetchCoordinate() {
     }
 }
 
-// function fillWeatherCard ()
-
-
-
-// fetchWeather();
-// async function fetchWeather(latitudeLongitude) {
-//     try {
-//         const urlLink = `${shortWeather}${latitudeLongitude}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`;
-//         console.log(urlLink);
-//         const responce = await fetch(urlLink);
-//         const weather = await responce.json();
-//         console.log(weather);
-//     }
-//     catch (error) {
-//         console.log("Error fetching weather: ", error);
-//     }
-// }
+function getWeatherByCode(weathercode) {
+    switch (weathercode) {
+        case 0:
+            return "Clear sky";
+        case 1:
+            return "Mainly clear";
+        case 2:
+            return "Partly cloudy";
+        case 3:
+            return "Overcast";
+        case 45:
+            return "Fog";
+        case 48:
+            return "Depositing rime fog";
+        case 51:
+            return "Drizzle: light intensity";
+        case 53:
+            return "Drizzle: moderate intensity";
+        case 55:
+            return "Drizzle: dense intensity";
+        case 56:
+            return "Freezing Drizzle: light intensity";
+        case 57:
+            return "Freezing Drizzle: dense intensity";
+        case 61:
+            return "Rain: slight intensity";
+        case 63:
+            return "Rain: moderate intensity";
+        case 65:
+            return "Rain: heavy intensity";
+        case 66:
+            return "Freezing Rain: light intensity";
+        case 67:
+            return "Freezing Rain: heavy intensity";
+        case 71:
+            return "Snow fall: slight intensity";
+        case 73:
+            return "Snow fall: moderate intensity";
+        case 75:
+            return "Snow fall: heavy intensity";
+        case 77:
+            return "Snow grains";
+        case 80:
+            return "Rain showers: slight";
+        case 81:
+            return "Rain showers: moderate";
+        case 82:
+            return "Rain showers: violent";
+        case 85:
+            return "Snow showers: slight";
+        case 86:
+            return "Snow showers: heavy";
+        case 95:
+            return "Thunderstorm";
+        case 96:
+            return "Thunderstorm with slight hail";
+        case 99:
+            return "Thunderstorm with heavy hail";
+    }
+}
+}
